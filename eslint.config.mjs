aca109670d2176470dafb226eae6192b4855980d
Iván Bautista <ivanbautistag@gmail.com>
2025-11-06 19:03:59 -0600

    env configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,70 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
+
+export default defineConfig([
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 100,
+          //ignorePattern: 'className="[^"]+"',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      quotes: [2, 'single', { avoidEscape: true }],
+    },
+  },
+])
+
+/*
+
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: ['tailwindcss'],
+    rules: {
+      'max-len': ['error', 100],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      quotes: [2, 'single', { avoidEscape: true }],
+      'tailwindcss/classnames-order': 'warn',
+    }
 
-export default eslintConfig;
+  },
+]
+
+export default eslintConfig
+*/
