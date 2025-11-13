de1b78191a7d517870a6dc22f6157523fc20f895
Iván Bautista <ivanbautistag@gmail.com>
2025-11-13 13:23:39 -0600

    feat: add hygraph client configuration and environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
+  env: {
+    schema: {
+      HYGRAPH_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+        default: "INFORM_VALID_TOKEN",
+      }),
+      HYGRAPH_API_URL: envField.string({
+        context: "server",
+        access: "secret",
+        optional: false,
+        default: "INFORM_VALID_TOKEN",
+      }),
+    },
+  },
 });
---
+++ src/lib/hygraph.ts
@@ -0,0 +1,12 @@
+import { GraphQLClient } from "graphql-request";
+import { HYGRAPH_API_URL, HYGRAPH_TOKEN } from "astro:env/server";
+const apiUrl = HYGRAPH_API_URL;
+const token = HYGRAPH_TOKEN;
+
+export const hygraphClient = new GraphQLClient(apiUrl, {
+  headers: token
+    ? {
+        Authorization: `Bearer ${token}`,
+      }
+    : {},
+});
