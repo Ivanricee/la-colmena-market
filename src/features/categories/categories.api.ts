9aaf0522984d906aab8124851f198c3d11f3a6b2
Iván Bautista <ivanbautistag@gmail.com>
2025-12-04 00:25:02 -0600

    feat: implement GraphQL query to fetch product categories from Hygraph

---
+++ src/features/categories/categories.api.ts
@@ -0,0 +1,37 @@
+import { hygraphClient } from "@/lib/hygraph";
+import type { Image } from "../products/products.model";
+import { gql } from "graphql-request";
+
+export interface Category {
+  id: string;
+  name: string;
+  slug: string;
+  description: { html: string };
+  image: Image[] | [];
+  isActive: boolean;
+}
+type categoriesResponse = {
+  categories: Category[] | [];
+  errorMessage?: string;
+};
+export const getCategories = async (): Promise<categoriesResponse> => {
+  try {
+    const { categories } = await hygraphClient.request(gql`
+      {
+        categories {
+          id
+          name
+          slug
+          description {
+            html
+          }
+          isActive
+        }
+      }
+    `);
+    return { categories };
+  } catch (error) {
+    console.log(error);
+    return { categories: [], errorMessage: "fallo al obtener las categorias" };
+  }
+};
