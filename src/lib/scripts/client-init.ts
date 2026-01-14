a637f6f82162bb0375d0f050e3efd4cc6efe5ce7
Iván Bautista <ivanbautistag@gmail.com>
2026-01-14 13:10:18 -0600

    refactor: replace Flowbite with Alpine.js plugin initialization

--- src/lib/scripts/client-init.ts
+++ src/lib/scripts/client-init.ts
@@ -1,20 +1,9 @@
-
-import { initInputNumber } from './inputNumber'
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
 
 export default function initClientUI(): void {
-  const setup = () => {
-    
-    initInputNumber()
+  if (!window.AlpineInstance) {
+    Alpine.plugin(collapse)
+    window.AlpineInstance = Alpine
   }
-
-  // Avoid running setup twice on the initial load because `astro:page-load`
-  // is also fired on first load when using View Transitions.
-  let lastUrl = window.location.href
-  setup()
-
-  document.addEventListener('astro:page-load', () => {
-    if (window.location.href === lastUrl) return
-    lastUrl = window.location.href
-    setup()
-  })
 }
