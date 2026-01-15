cd850464bbfb42ca7ab527bb457ace3d55f478f9
Iván Bautista <ivanbautistag@gmail.com>
2026-01-15 13:06:40 -0600

    types for alpine/nanostore  cart store

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,21 @@
+import type { Cart } from '@/store/cartStore'
+
 declare global {
   interface Window {
     AlpineInstance?: any
   }
+}
+
+declare module 'alpinejs' {
+  interface Alpine {
+    store(name: 'cartStore', value: CartStoreType): void
+    store(name: string): any
+  }
+}
+
+interface CartStoreType {
+  data: Cart
+  init(): void
 }
 
 declare module '@alpinejs/collapse' {
