e8ebbc50eaa19f3f78f59bd71bec291c813891d5
Iván Bautista <ivanbautistag@gmail.com>
2026-02-17 21:26:05 -0600

    feat: add step navigation methods and fix totalPrice formatting in cart handler

--- src/lib/scripts/cart.ts
+++ src/lib/scripts/cart.ts
@@ -1,3 +1,5 @@
+import { handleNextStep, proceedFromStep } from '../utils/cart.order'
+
 type AlpineLike = {
   data: (name: string, callback: () => any) => void
   store: (name: string) => any
@@ -13,7 +15,15 @@ export const setupCartHandler = (Alpine: AlpineLike) => {
       return Object.keys(Alpine.store('cartStore')?.data?.items ?? {}).length
     },
     get totalPrice() {
-      return Object.keys(Alpine.store('cartStore')?.data?.total ?? 0)
+      return (Alpine.store('cartStore')?.data?.total ?? 0).toFixed(2)
+    },
+    canProceedFromStep() {
+      return proceedFromStep({ step: this.step, items: this.items })
+    },
+    nextStep() {
+      console.log('nextStep aaaa')
+      const nextStep = handleNextStep({ step: this.step })
+      this.setStep(nextStep)
     },
     setStep(step: number) {
       this.step = step
